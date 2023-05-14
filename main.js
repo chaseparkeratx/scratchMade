1bae26786a354f6afa49c6062d034b1798a238e2
chaseparkeratx <112991010+chaseparkeratx@users.noreply.github.com>
2023-05-14 12:40:25 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,105 @@
+let i = 1;
+do {
+  console.log(i);
+  i++;
+} while (i <= 100);
+
+const people = [
+  {"firstName":"Denny",
+    "lastName":"Ilewicz",
+    "gender":"Male",
+    "birthDate":"6/28/2002"},
+  {
+    "firstName":"Chere",
+    "lastName":"Crookshank",
+  "gender":"Female",
+  "birthDate":"8/6/1998"},
+{
+  "firstName":"Zak",
+  "lastName":"Sybe",
+  "gender":"Male",
+  "birthDate":"5/30/1981"},
+{
+  "firstName":"Romola",
+  "lastName":"Onyon",
+  "gender":"Female",
+  "birthDate":"9/22/1995"},
+{
+  "firstName":"Kristine",
+  "lastName":"Leyburn",
+  "gender":"Female",
+  "birthDate":"9/7/2004"
+}]
+
+// console.log(people)
+
+// const logKeys = (keys) => {
+//   let key = people.filter(firstName => {
+//     for(i = 0; i > firstName.length; i++)
+//     if(i === "Jane")
+//     return key
+//     logKeys(key)
+//   } 
+//   )
+// }
+// console.log(logKeys(key))
+// logKeys(people)
+
+// const gender = people.filter(person => person.birthDate > 
+// )
+
+// console.log(gender)
+
+
+
+
+
+
+
+
+
+
+
+// class person {
+//   constructor(firstName, lastName, birthDate, gender) {
+//     this.firstName = firstName
+//     this.lastName = lastName
+//     this.birthDate = birthDate
+//     this.gender = gender
+//   }
+// }
+
+
+// const logKeys = (keys) => {
+//   person.filter((name) => {
+//     for(i = 0; i > firstName.length; i++)
+//     if(i === "Jane")
+//     return firstName
+//   } 
+//   )
+//   console.log(Object.keys(person))
+// }
+
+// const logValues = (values) => {
+//   console.log(Object.entries(person))
+// }
+
+// let odd = arrayOfPersons.map((person) => {
+//   for(let i = 0; i < arrayOfPersons.birthDate.length; i++)
+//   if(i < 1990) {
+//     console.log(oddBirthdate(arrayOfPersons))
+//   } else ""
+// })
+
+// const oddBirthDate = (birthDate) => {
+//   let odd = arrayOfPersons.map((person) => {
+//     for(let i = 0; i < arrayOfPersons.birthDate.length; i++)
+//     if(i < 1990) {
+//       console.log(oddBirthdate(arrayOfPersons))
+//     } else ""
+//   })
+//   console.log(odd)
+// }
+
+// console.log(logKeys(person))
+// console.log(logValues(person))
